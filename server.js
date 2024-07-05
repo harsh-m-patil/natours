d1df1fd68da48dd538c0311b81497c0953643151
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-05 14:17:59 +0530

    Database connected
    connected Database using mongoose and created a collection and a document

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
+/* eslint-disable no-console */
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({ path: "./config.env" }); // should be before app
 
-const app = require("./app");
+mongoose.connect(process.env.DB_URI, {}).then(() => {
+  console.log("Database connected");
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"], // arr[1] error message
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"], // arr[1] error message
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "Test",
+  rating: 4.7,
+  price: 232,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("ERROR", err);
+  });
+
 // (4) SERVER
 //console.log(app.get("env")); // set by express
 
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}`);
 });
