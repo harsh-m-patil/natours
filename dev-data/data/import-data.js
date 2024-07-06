d14f2876def21ca5ade4f8dd5dfd553393c0fa10
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-06 18:05:35 +0530

    Added more fields to tour Model
    imported data in database

---
+++ dev-data/data/import-data.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const mongoose = require("mongoose");
+const fs = require("fs");
+const Tour = require("../../model/tourModel");
+
+const DB_URI = "mongodb://127.0.0.1:27017/natours";
+
+mongoose.connect(DB_URI, {}).then(() => {
+  console.log("DB Connected");
+});
+
+const tours = JSON.parse(fs.readFileSync("./tours-simple.json", "utf-8"));
+
+// Import DATA into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("data loaded");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// Delete All DATA from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("data deleted");
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
--- model/tourModel.js
+++ model/tourModel.js
@@ -5,15 +5,54 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"], // arr[1] error message
     unique: true,
+    trim: true, // removes whitespace in beginning and end
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group Size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a group difficulty"],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"], // arr[1] error message
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true, // removes whitespace in beginning and end
+    required: [true, "A tour must have summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
