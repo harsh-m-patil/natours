ee2b17a691490c8f5f4c7bf3ec1e1495e981b552
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-08 13:48:05 +0530

    Added field limiting and pagination to api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,8 +16,37 @@ exports.getAllTours = async (req, res) => {
       (match) => `$${match}`,
     );
 
-    const query = Tour.find(JSON.parse(queryString));
+    let query = Tour.find(JSON.parse(queryString));
 
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query.sort("-createdAt");
+    }
+
+    // Fields Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query.select("-__v");
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=3&limit=10
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) {
+        throw new Error("This page does not exist");
+      }
+    }
     // Execute Query
     const tours = await query;
 
