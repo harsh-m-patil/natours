27ce547509d3402c0bef27a5f1e651616a3bd458
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-11 13:41:50 +0530

    Added validators

--- model/tourModel.js
+++ model/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"], // arr[1] error message
       unique: true,
       trim: true, // removes whitespace in beginning and end
+      maxlength: [40, "A tour name must have less or equal 40 characters"],
+      minlength: [10, "A tour name must have more or equal 10 characters"],
     },
     slug: {
       type: String,
@@ -23,18 +25,28 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        // only for strings
+        values: ["easy", "medium", "difficult"],
+        message: "Invalid difficulty type,must be either easy,medium,difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      // for numbers and date
+      min: [1, "A ratings must be greater than 1.0"],
+      max: [1, "A ratings must be less than 5.0"],
     },
     ratingQuantity: {
       type: Number,
+      min: [0, "A ratingQuantity must be positive "],
       default: 0,
     },
     price: {
       type: Number,
       required: [true, "A tour must have a price"], // arr[1] error message
+      min: [0, "A tour price must be positive "],
     },
     priceDiscount: {
       type: Number,
