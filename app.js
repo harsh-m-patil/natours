b5082b86ca68372e03ee0d2e7ab788db289078e0
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-11 16:50:04 +0530

    Added basic global error handling

--- app.js
+++ app.js
@@ -18,10 +18,26 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// unhandled paths
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl}`,
+  //res.status(404).json({
+  //  status: "fail",
+  //  message: `Can't find ${req.originalUrl}`,
+  //});
+  const err = new Error(`Can't find ${req.originalUrl}`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
