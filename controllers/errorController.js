25fdceb23b091a66a48f37e69ce1a312756d4de6
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-13 10:45:39 +0530

    Handled Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -14,9 +20,6 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
   } else {
-    // eslint-disable-next-line no-console
-    console.log(err);
-
     res.status(500).json({
       status: "error",
       message: "Something Went Wrong",
@@ -31,6 +34,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === "CastError") {
+      error = handleCastErrorDB(err);
+    }
+    sendErrorProd(error, res);
   }
 };
