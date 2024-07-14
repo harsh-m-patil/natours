0ef99d79b27c0cd45f008044eef5f8fc4837bf0f
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-07-14 19:28:18 +0530

    Handle Unhandled Rejections

--- server.js
+++ server.js
@@ -8,11 +8,20 @@ dotenv.config({ path: "./config.env" }); // should be before app
 mongoose.connect(process.env.DB_URI, {}).then(() => {
   console.log("Database connected");
 });
+// .catch()
 
 // (4) SERVER
 //console.log(app.get("env")); // set by express
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("unhandledRejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
