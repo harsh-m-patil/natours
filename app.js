21b37107be386d60338841db7a84f2e09a711c6e
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-07 01:05:46 +0530

    Add data sanitization
    using express-mongo-santize and xss-clean

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -36,6 +38,12 @@ app.use(
     limit: "10kb",
   }),
 ); // get request params
+
+// DATA santization against NOSQL query injection
+app.use(mongoSanitize());
+
+// DATA santization against XSS
+app.use(xss());
 
 // SERVE static files
 app.use(express.static(`${__dirname}/public`));
