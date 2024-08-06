e84d1e7b270d0a37bdc68db06f3c354516242d8c
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-07 00:34:46 +0530

    Add secure http headers
    using helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -11,10 +12,16 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // (1) GLOBAL Middlerware
+
+// SET security HTTP Headers
+app.use(helmet());
+
+// DEV LOGGING
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
+// LIMIT requests from same API
 const limiter = rateLimit({
   max: 100, // max no of reqs
   windowMs: 60 * 60 * 1000, // window size of max requests
@@ -23,14 +30,22 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-//using middleware to get access to body
-app.use(express.json()); // get request params
+//using middleware to get access to body (body-parser)
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+); // get request params
+
+// SERVE static files
 app.use(express.static(`${__dirname}/public`));
 
+// TEST Middlerware
 app.use((req, res, next) => {
   req.responseTime = new Date().toISOString();
   next();
 });
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
