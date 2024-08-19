cf884a1220712d3e9f77cdf3952df1f8697b72bf
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-19 23:18:18 +0530

    populate tour guides

--- model/tourModel.js
+++ model/tourModel.js
@@ -147,6 +147,13 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+  next();
+});
 //tourSchema.post(/^find/, (docs, next) => {
 //  console.log(docs);
 //  next();
