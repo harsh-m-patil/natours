d6aafc5546ba8bb6a60267beb2563fbfc7b04709
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-10-28 18:45:06 +0530

    feat/add calculate avg Rating

--- model/reviewModel.js
+++ model/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,6 +48,47 @@ reviewSchema.pre(/^find/, function (next) {
     select: "name photo",
   });
   next();
+});
+
+// Static Methods availabe on model
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // NOTE: this points to current Model
+  // Calculate avg review
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour", // by what do you want to group
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsAverage: stats[0].avgRating,
+    ratingQuantity: stats[0].nRating,
+  });
+};
+
+reviewSchema.post("save", function () {
+  // this points to current review
+
+  // this.constructor
+  this.constructor.calcAverageRatings(this.tour);
+  //NOTE: Post middleware does not have access to next
+  //next();
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.model.findOne();
+  next();
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
